1b60a314cf8c70156dc963c909d6a2a92275a700
Wagner Arcieri <wagner.arcieri@gmail.com>
2023-05-30 20:52:16 -0300

    fix: text error fixed, added colors to easy visualization

--- app/src/main/java/br/com/bluetoothchat/presentation/components/DeviceScreen.kt
+++ app/src/main/java/br/com/bluetoothchat/presentation/components/DeviceScreen.kt
@@ -11,9 +11,11 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -41,11 +43,21 @@ fun DeviceScreen(
             modifier = Modifier.fillMaxWidth(),
             horizontalArrangement = Arrangement.SpaceAround
         ) {
-            Button(onClick = onStartScan) {
+            Button(
+                onClick = onStartScan,
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = Color(0xFF018A01)
+                )
+            ) {
                 Text("Start SCAN")
             }
-            Button(onClick = onStopScan) {
-                Text("Start SCAN")
+            Button(
+                onClick = onStopScan,
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = Color(0xFF5E0000)
+                )
+            ) {
+                Text("Stop SCAN")
             }
         }
     }
